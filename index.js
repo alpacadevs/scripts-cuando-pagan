7e58ac6544f1ec4d5b3e01473e6262da103d8510
madKoding <contacto@madkoding.com>
2021-06-30 10:53:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,32 @@ var moment = require('moment-business-days')
 moment.locale('es')
 
 module.exports = function(app) {  
-  // All the room in the world for your code
-app.message(/cuando pagan/, async ({ command, say }) => {  
-  try {
-    var today = moment(`${moment().format('YYYY-MM-DD')}T00:00:00-04:00`)
-    var lastBusinessDayMoment = moment()
-      .endOf('month')
-      .isBusinessDay()
-      ? moment().endOf('month')
-      : moment()
+  app.message(/cuando pagan/, async ({ command, say }) => {  
+    try {
+      var today = moment(`${moment().format('YYYY-MM-DD')}T00:00:00-04:00`)
+      var lastBusinessDayMoment = moment()
         .endOf('month')
-        .prevBusinessDay()
-    var dateLastBusinessDay = lastBusinessDayMoment.format('YYYY-MM-DD')
-    var lastBusinessDay = moment(`${dateLastBusinessDay}T00:00:00-04:00`)
-    var dayMessage = moment.duration(lastBusinessDay.diff(today)).humanize()
-    var dayCount = lastBusinessDay.diff(today, 'days')
-    var message = ''
-    var plural = dayCount > 1 ? 'n' : ''
-    if (dayCount === 0) {
-      message = ':tada: Hoy pagan :tada:'
-    } else {
-      message = `Falta${plural} ${dayMessage} para que paguen. Este mes pagan el ${lastBusinessDay.format(
-        'D'
-      )}, que cae ${lastBusinessDay.format('dddd')} :tired_face:`
+        .isBusinessDay()
+        ? moment().endOf('month')
+        : moment()
+          .endOf('month')
+          .prevBusinessDay()
+      var dateLastBusinessDay = lastBusinessDayMoment.format('YYYY-MM-DD')
+      var lastBusinessDay = moment(`${dateLastBusinessDay}T00:00:00-04:00`)
+      var dayMessage = moment.duration(lastBusinessDay.diff(today)).humanize()
+      var dayCount = lastBusinessDay.diff(today, 'days')
+      var message = ''
+      var plural = dayCount > 1 ? 'n' : ''
+      if (dayCount === 0) {
+        message = ':tada: Hoy pagan :tada:'
+      } else {
+        message = `Falta${plural} ${dayMessage} para que paguen. Este mes pagan el ${lastBusinessDay.format(
+          'D'
+        )}, que cae ${lastBusinessDay.format('dddd')} :tired_face:`
+      }
+      say(message)
+    } catch (error) {
+      console.error(error);   
     }
-    say(message)
-  } catch (error) {
-    console.log("err")
-    console.error(error);   
-  }
-});
+  });
 }
-
